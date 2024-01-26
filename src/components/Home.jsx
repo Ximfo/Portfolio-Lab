04908aa7eada017bf6402cbd83b2a5a946b4bfd5
Ximfo <Ximffo@gmail.com>
2024-01-26 20:20:25 +0100

    header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Header } from "./Header";
-
+import { MainSection } from "./MainSection";
 
 export const Home=()=> {
     return (
-        <>
-        <Header />
-        </>
+        <div className="container">
+            <Header />
+            <MainSection />
+        </div>
     )
 }
 
---
+++ src/components/MainSection.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const MainSection =()=> {
+    return (
+        <div id="mainsection">
+           <h1>abc</h1>
+        </div>
+    )
+}
+
